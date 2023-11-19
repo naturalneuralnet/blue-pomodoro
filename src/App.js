bcbc0697d62b33467d0f3d3ca85031bca625cd42
naturalneuralnet <natneuralnet@outlook.com>
2023-11-19 16:05:56 +0000

    updating app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   // change the icon, must be an icon in assets
 
   const link = document.querySelector('link[rel="icon"]');
-  link.setAttribute("href", "./sand-timer.ico");
+  link.setAttribute("href", "./assets/sand-timer.ico");
 
   // pauses the video when window is not in view
   // courtesy of LogRocket :https://blog.logrocket.com/optimizing-video-backgrounds-css-javascript/#making-video-backgrounds-responsive
